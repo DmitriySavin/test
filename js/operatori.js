dfbe2d24859cabfe52e8711ed2c162c9ca46ac99
DmitriySavin <debilv556@gmail.com>
2024-12-07 14:19:52 +0100

    add hw 2

--- js/operatori.js
+++ js/operatori.js
@@ -1,23 +1,43 @@
-// const x = 7;
-// const y = 5;
-// console.log(x + y);
-// let value = 5;
-// value += 10;
-// console.log(value);
-// const hello = 'hello';
-// console.log('hello' === 'hello');
-const valueA = '5';
-console.log(Number(valueA));
-console.log(typeof Number(valueA));
+// 1
+let celsia = 30;
+let farengeit = (celsia * 9 / 5) + 32;
+console.log('celsia:',celsia,'farengeit:',farengeit);
+// я если честно не знал бы что это за формула..
 
-const valueB = 'five';
-console.log(Number(valueB));
-// console.log(typeof Number(valueB));
+// 2
+const hours = (30 * 24);
+const seconds = (30 * 60);
+console.log('Часи:', hours, 'Хвилини:', seconds);
 
-console.log(Number.parseFloat('2.5px'));
-// 
-const validNumber = Number('52');
-console.log(Number.isNaN(validNumber));
+// 3
+let healt = 100;
+let energy = 100;
+console.log('Healt 100 /', healt - 20, 'Energy 100 /', energy - 60);
 
-const invalidNumber = Number('abg');
-console.log(Number.isNaN(invalidNumber));
+// 4
+let sum = "100";
+let discount = sum * 0.10;
+let discountSum = sum - discount;
+console.log(discountSum);
+
+// 5
+const x = 10.5343242;
+const y = Math.floor(x)
+console.log(y)
+
+// 6
+let z = "22.53adsdds";
+console.log(Number.parseFloat(z));
+
+// 7
+let c = "22.53adsdds";
+console.log(Number.parseInt(c));
+
+// 8
+let v = 10;
+console.log(Math.sqrt(v))
+
+// 9
+let a = 10.16;
+a = (Number.parseInt(a));
+console.log(a.toString())
