2d4fdc515158723a330e50b349d65f393476b4d2
DmitriySavin <debilv556@gmail.com>
2024-12-12 00:13:35 +0100

    add

--- js/operatori.js
+++ js/operatori.js
@@ -1,43 +1,45 @@
-// 1
-let celsia = 30;
-let farengeit = (celsia * 9 / 5) + 32;
-console.log('celsia:',celsia,'farengeit:',farengeit);
-// я если честно не знал бы что это за формула..
+// // 1
+// let celsia = 30;
+// let farengeit = (celsia * 9 / 5) + 32;
+// console.log('celsia:',celsia,'farengeit:',farengeit);
+// // я если честно не знал бы что это за формула..
 
-// 2
-const hours = (30 * 24);
-const seconds = (30 * 60);
-console.log('Часи:', hours, 'Хвилини:', seconds);
+// // 2
+// const hours = (30 * 24);
+// const seconds = (30 * 60);
+// console.log('Часи:', hours, 'Хвилини:', seconds);
 
-// 3
-let healt = 100;
-let energy = 100;
-console.log('Healt 100 /', healt - 20, 'Energy 100 /', energy - 60);
+// // 3
+// let healt = 100;
+// let energy = 100;
+// console.log('Healt 100 /', healt - 20, 'Energy 100 /', energy - 60);
 
-// 4
-let sum = "100";
-let discount = sum * 0.10;
-let discountSum = sum - discount;
-console.log(discountSum);
+// // 4
+// let sum = "100";
+// let discount = sum * 0.10;
+// let discountSum = sum - discount;
+// console.log(discountSum);
 
-// 5
-const x = 10.5343242;
-const y = Math.floor(x)
-console.log(y)
+// // 5
+// const x = 10.5343242;
+// const y = Math.floor(x)
+// console.log(y)
 
-// 6
-let z = "22.53adsdds";
-console.log(Number.parseFloat(z));
+// // 6
+// let z = "22.53adsdds";
+// console.log(Number.parseFloat(z));
 
-// 7
-let c = "22.53adsdds";
-console.log(Number.parseInt(c));
+// // 7
+// let c = "22.53adsdds";
+// console.log(Number.parseInt(c));
 
-// 8
-let v = 10;
-console.log(Math.sqrt(v))
+// // 8
+// let v = 10;
+// console.log(Math.sqrt(v))
 
-// 9
-let a = 10.16;
-a = (Number.parseInt(a));
-console.log(a.toString())
+// // 9
+// let a = 10.16;
+// a = (Number.parseInt(a));
+// console.log(a.toString())
+// let sectence = 'i love js';
+// console.log(sectence.length)
---
+++ js/radki.js
@@ -0,0 +1,18 @@
+let result = 5 + 5 + '5';
+console.log(typeof(result), result);
+
+let email = "dimasavin10@gmail.com";
+let check = email.includes('@');
+console.log(check, email.length);
+
+let my = "my,";
+let name = 'name,';
+let is = 'is';
+let fullName = 'Viktor';
+console.log(`my , name , is ${fullName}`)
+
+let userName = 'Олександр';
+let payment = '100 гривень';
+const msg = alert(`Дякуємо, ${userName}! До сплати ${payment}`);
+const paid = 'Оплачено';
+console.log(paid)
