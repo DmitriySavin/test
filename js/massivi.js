fb550fe8737702c2a557a680bc8db7aacc5a9646
DmitriySavin <debilv556@gmail.com>
2025-01-11 14:46:09 +0100

    add

---
+++ js/massivi.js
@@ -0,0 +1,48 @@
+const a = [1, 2, 3];
+a[1] = 5;
+console.log(a)
+// 
+const string = ['a', 'b', 'c'];
+string.length = 3;
+string[3] = 'd';
+console.log(string); 
+// 
+const num = [10, 35, 46, 57];
+let res = 0;
+for (let number of num) {
+    res = res + number;
+}
+console.log(res);
+//
+const arr = [1, 2, 3, 4, 5];
+for (let array of arr) {
+    console.log(array)
+}
+//
+const arr2 = ['apple', 'banana', 'Orange', 'Pear', 'Lemon'];
+for (let array2 of arr2) {
+    if (array2.length > 5) {
+console.log(array2)
+    }
+}
+// 
+const arr3 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log(arr3[9]);
+// Ну типо это ведь можно было сделать в таком виде ? 2 строки
+
+const arr4 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+for (let array4 of arr4) {
+    if (array4 % 2 === 0) {
+        console.log(array4)
+    }
+}
+
+
+
+
+
+
+
+
+
+
