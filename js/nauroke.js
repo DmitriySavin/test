b65b3433235dbe3b4866cfdb27b4b875fc2f002b
DmitriySavin <debilv556@gmail.com>
2025-03-09 01:28:08 +0200

    add

--- js/nauroke.js
+++ js/nauroke.js
@@ -5,3 +5,27 @@
 
 // const [a,b,c, ...rest] = arr1;
 // console.log(rest);
+const cart = [
+  {
+    label: "apples",
+    price: 100,
+    quantity: 2,
+    // 200
+  },
+  {
+    label: "bananas",
+    price: 120,
+    quantity: 3,
+    // 360
+  },
+  {
+    label: "lemons",
+    price: 70,
+    quantity: 5,
+    // 350
+  },
+];
+const totalAmount =  cart.reduce((acc, {price , quantity}) => acc + price * quantity, 0);
+// const totalAmount = cart.reduce((acc, item) => acc + item.price * item.quantity,0);
+// const { label, price, quantity } = item;
+console.log(totalAmount);
--- js/test.js
+++ js/test.js
@@ -215,15 +215,14 @@ const hotel5 = {
       big: 145,
     },
   },
-}; 
+};
 // const {small,medium,big} = hotel.rooms.sizes;
 // const {sizes, ...others} = hotel;
 // const {sizes {small,medium,big} } = hotel.rooms;
 
-
 //  //  //  //  //
 // императивный код
-const numbers1 = [1,2,3,4,5];
+const numbers1 = [1, 2, 3, 4, 5];
 // const arr2 = [];
 // for (let i = 0; i < numbers1.lenght; i++) {
 //   if(numbers[i] > 5) {
@@ -232,27 +231,27 @@ const numbers1 = [1,2,3,4,5];
 // }
 
 // декларативный код
-const newNumbers = numbers1.filter(item => item > 5);
+const newNumbers = numbers1.filter((item) => item > 5);
 // filter - перебор массива
 
 // чистые функции;
 
-const numbers3 = [1,2,3,4,5,6];
+const numbers3 = [1, 2, 3, 4, 5, 6];
 const dirtyFunction = (array, value) => {
-  for(let i = 0; i < array.lenght; i++) {
+  for (let i = 0; i < array.lenght; i++) {
     console.log(array[i]);
     array[i] = array[i] * value;
   }
   return array;
-}
-console.log(dirtyFunction(numbers,2));
+};
+console.log(dirtyFunction(numbers, 2));
 // const result1 = dirtyFunction(numbers, 2);
 // const result2 = dirtyFunction(numbers)
 
 const clearFunction = (array, value) => {
   const copyArray = [];
-  for(let i = 0; i < array.lenght; i++) {
- copyArray.push(array[i] * value) ;
+  for (let i = 0; i < array.lenght; i++) {
+    copyArray.push(array[i] * value);
   }
   return copyArray;
 };
@@ -268,59 +267,91 @@ const result4 = clearFunction(numbers, 3);
 // names.forEach(function(item, index) {
 //   console.log(`${index + 1} - ${item}`);
 // })
-names.forEach((item,index) => console.log(`${index + 1} - ${item}`));
+names.forEach((item, index) => console.log(`${index + 1} - ${item}`));
 
-// map
+// map - перебирает массив выполняя условие
 
-const prices = [50,100,150,200,250];
+const prices = [50, 100, 150, 200, 250];
 // const newPrices = [];
 // for(const price of prices) {
 // newPrices.push(price * 0.7);
 // }
 
-const newPrices = prices.map(price => price * 0.7);
+const newPrices = prices.map((price) => price * 0.7);
 console.log(newPrices);
 const inventors = [
   {
-    name: 'Albert',
-    surname: 'Einstein',
+    name: "Albert",
+    surname: "Einstein",
     born: 1879,
     passed: 1955,
   },
   {
-    name: 'Isaac',
-    surname: 'Newton',
+    name: "Isaac",
+    surname: "Newton",
     born: 1643,
     passed: 1727,
   },
   {
-    name: 'Galileo',
-    surname: 'Galilei',
+    name: "Galileo",
+    surname: "Galilei",
     born: 1564,
     passed: 1642,
-  }
+  },
 ];
-const names = inventors.map(inventor => inventors.name);
+const names = inventors.map((inventor) => inventors.name);
 
 // filter
 
-const numbers2 = [1,2,3,4,5,6,7,8,9,0];
-const num2 = numbers2.filter(num => num > 5);
+const numbers2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+const num2 = numbers2.filter((num) => num > 5);
 console.log(num2);
 
 //                  // .isActive
 
-const numbers5 = [1,2,3,4,5,-6];
-const positiveNumbers = numbers5.filter(number => number > 0);
+const numbers5 = [1, 2, 3, 4, 5, -6];
+const positiveNumbers = numbers5.filter((number) => number > 0);
 
-// find
+// find - перебирает массив и находит хначение подходящее под условие
 
-const numb1 = [1,2,3,4,5,6];
-const positnumb1 = numb1.find(num => num < 5);
+const numb1 = [1, 2, 3, 4, 5, 6];
+const positnumb1 = numb1.find((num) => num < 5);
 
 // every && come
-const numb2 = [1,2,3,4,5,6];
-const positnumb2 = numb1.every(num => num < 5);
+// 1 - перебирая массив выдает все обьекты подходящие под условие
+// 2 - перебирая массив выдает первый обьект подходящий под условие
+const numb2 = [1, 2, 3, 4, 5, 6];
+const positnumb2 = numb1.every((num) => num < 5);
 
-const numb3 = [1,2,3,4,5,6];
-const positnumb3 = numb1.some(num => num < 5);
+const numb3 = [1, 2, 3, 4, 5, 6];
+const positnumb3 = numb1.some((num) => num < 5);
+
+// reduce
+
+const numbers6 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const total = numbers.reduce((acc, number) => {
+  return acc + number;
+}, initialValue);
+
+// const total = numbers.reduce((acc , number) => acc + number, 0);
+
+// 
+//  
+const numbers7 = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
+const total1 = numbers7.reduce((acc, number) => acc + number ,0);
+
+
+const salaryes = {
+  vasya: 4200,
+  dima: 3440,
+  anton: 4444,
+  artem: 1250
+};
+
+const totalsalary = Object.values(salaryes.reduce((acc, salary) => acc + salary, 0));
+
+//  sort
+
+const numbers8 = [1,2,3,4,5];
+numbers8.sort((a,b) => a - b);
+numbers.sort((a,b) => a.localeCompare(b));
